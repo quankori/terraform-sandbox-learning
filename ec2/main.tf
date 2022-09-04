ffe8a0c4efe0a39c64fcbfa6307d43bafc17b3d4
Quan Kori <k0r1@Quans-MacBook-Pro-2.local>
2022-09-04 23:14:42 +0700

    Ec2 with terraform

---
+++ ec2/main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "kori_test" {
+  instance_type = "t2.micro"
+  ami           = "ami-02f3416038bdb17fb"
+  tags = {
+    Name = "KoriTag"
+  }
+}
